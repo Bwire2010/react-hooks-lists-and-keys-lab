73fd3c7a925f6868334e34d6f6fda0dfbc575350
Patrobas Bwire <patrobasbwire@gmail.com>
2022-09-21 10:06:56 +0300

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,21 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkList = links.map( ( link ) =>
+  {
+    return (
+      <a key={ link } href={ "#" + link } >{ link }</a>
+      )
+  } );
+
+  return (
+  <nav>{/*
+    <a href="#home">home</a>
+    <a href="#about">about</a>
+    <a href="#projects">projects</a>*/}
+    {linkList}
+  </nav>
+  );
 }
 
 export default NavBar;
